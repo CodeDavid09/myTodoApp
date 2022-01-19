6e07053b0ae23e091ab37c8640e410b788b60d21
David Gavino <ggavino2@uccs.edu>
2022-01-18 21:30:58 -0700

    modified server.js

--- server.js
+++ server.js
@@ -10,6 +10,36 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// User APIs
+app.post('/api/users', async (req, res) => {
+    const {username} = req.body;
+
+    if(!username) {
+        return res.status(400).json({error: 'You must provide username'});
+    }
+    try {
+        const createUserQuery = 'INSERT INTO users(username) VALUES(?);';
+        const getUserByIdQuery = 'SELECT * FROM users WHERE id = ?;';
+
+        const [result] = await connection.query(createUserQuery, [username]);
+        const [userResult] = await connection.query(getUserByIdQuery, [result.insertId]);
+        res.json(userResult[0]);
+    } catch (e) {
+        res.status (400).json (e);
+    }
+
+});
+
+app.get('/api/todos', async (req, res) => {
+    try {
+    const getAllTodosQuery = 'SELECT * FROM todos;';
+    const [todos] = await connection.query(getAllTodosQuery);
+    res.json(todos);
+    } catch (e) {
+        res.status(400).json(e);
+    }
+});
+
 // POST - create todo
 // ASYNC -declaring a function as "async" allows us to use "await" syntax within that function
 app.post('/api/todos', async (req, res) => {
